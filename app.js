a3828d6462504ab2dfde70855ccc80b9790817b1
HASSAN <tahirniacci009@gmail.com>
2024-03-18 12:52:18 +0500

    Hello world Server is running

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
+// const routes = require("./routes");
+
+// Routes
+// app.use("/", routes);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // Path: app.js
+module.exports = app;
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const app = require("./app");
 const port = 3000;
+
+server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
