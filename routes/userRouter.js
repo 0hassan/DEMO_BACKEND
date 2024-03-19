2a167af457d71b9748a3c028755ce75ed3eea16d
HASSAN <tahirniacci009@gmail.com>
2024-03-19 14:15:16 +0500

    user routes private

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,15 +2,16 @@ const express = require("express");
 const router = express.Router();
 const validate = require("../middleware/validate");
 const { userValidation } = require("../validations");
+const auth = require("../middleware/auth");
 
 // Path: controllers/userController.js
 const { userController } = require("../controllers/index");
 
 router
   .route("/:email")
-  .get(validate(userValidation.getUser), userController.getUserByEmail)
-  .put(userController.updateUser)
-  .delete(userController.deleteUser);
+  .get(validate(userValidation.getUser), auth, userController.getUserByEmail)
+  .put(auth, userController.updateUser)
+  .delete(auth, userController.deleteUser);
 
 router
   .route("/")
